a35b91b65bcdf5de46622b17dc6b7fb4af07dcb8
Adam Kadlec <adam.kadlec@gmail.com>
2022-01-03 12:49:39 +0100

    Updating packages

--- ws_server_plugin/__init__.py
+++ ws_server_plugin/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 #     Copyright 2021. FastyBird s.r.o.
 #
 #     Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,4 +18,4 @@
 WS server for real time communication between clients and server
 """
 
-__version__ = "0.2.15"
+__version__ = "0.2.16"
